3ccff2811519faa3cdce9bc18117702cbfbcc1e0
Дмитрий Малофеев <dim2k2006@yandex.ru>
2019-09-26 22:14:32 +0300

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
+const NODE_ENV = process.env.NODE_ENV || 'development';
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: process.env.NODE_ENV || 'development',
+  mode: NODE_ENV === 'development' ? 'development' : 'production',
+
   module: {
     rules: [
       {
@@ -11,15 +15,59 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
     ],
   },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: 'template.html',
     }),
   ],
 };
+
+if (NODE_ENV === 'development') {
+  module.exports.module.rules.push(
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+    },
+  );
+}
+
+if (NODE_ENV === 'production') {
+  module.exports.plugins.push(
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        sourceMap: false,
+        beautify: false,
+        comments: false,
+        mangle: {
+          keep_fnames: true,
+        },
+        compress: {
+          sequences: true,
+          booleans: true,
+          loops: true,
+          unused: true,
+          drop_console: true,
+        },
+      },
+    }),
+  );
+
+  module.exports.plugins.push(
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  );
+
+  module.exports.module.rules.push(
+    {
+      test: /\.css$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        'css-loader',
+      ],
+    },
+  );
+}
